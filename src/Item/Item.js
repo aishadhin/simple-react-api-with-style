43d5f298342e56c7cbd003a1da850c826d93f0f3
aishadhin <aishadhin.ac@gmail.com>
2022-03-31 20:32:32 +0600

    db

--- src/Item/Item.js
+++ src/Item/Item.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { added } from '../fakedb';
+import { added, removeFromDb } from '../fakedb';
 import './Item.css'
 
 
@@ -8,12 +8,18 @@ const Item = (props) => {
     const add = id => {
         added(id)
     }
+
+    const removeItem = id => {
+        removeFromDb(id)
+    }
+
     return (
         <div className='items'>
             <h3>Country Name: {props.country.name.common}</h3>
             <h4>Area Code: {props.country.area}</h4>
             <img src={props.country.flags.png} alt="" />
-            <button className='btn' onClick={()=>add(id)}>Add To Storage</button>
+            <button onClick={()=>add(id)}>Add To Storage</button>
+            <button onClick={()=>removeItem(id)}>Remove Item</button>
         </div>
     );
 };
--- src/fakedb.js
+++ src/fakedb.js
@@ -1,10 +1,31 @@
 const added = id => {
-    const getItem = localStorage.getItem(id);
-    if (getItem) {
-        const num = parseInt(getItem)
-        localStorage.setItem(id, num + 1)
+    let obj;
+    let upCart = localStorage.getItem('cart');
+    if (upCart) {
+        obj = JSON.parse(upCart)
     } else {
-        localStorage.setItem(id, 1)
+        obj = {};
+    }
+
+    const quantity = obj[id];
+    if (quantity) {
+        const upQuan = quantity + 1;
+        obj[id] = upQuan;
+    } else {
+        obj[id] = 1;
+    }
+    localStorage.setItem('cart', JSON.stringify(obj));
+    // console.log(setCart)
+}
+
+const removeFromDb = id => {
+    const ckForRem = localStorage.getItem('cart');
+    if (ckForRem) {
+        const objRem = JSON.parse(ckForRem);
+        if (id in objRem) {
+            delete objRem[id];
+            localStorage.setItem('cart', JSON.stringify(objRem));
+        }
     }
 }
-export {added}
+export { added, removeFromDb }
